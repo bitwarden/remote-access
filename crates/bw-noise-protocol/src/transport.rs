77c50a2cd022055fc7a2c26f5c4f04dd1c83d59a
Katherine Reynolds <7054971+reynoldskr@users.noreply.github.com>
2026-03-11 11:28:38 -0700

    Updated for system time compatibility in WASM

--- crates/bw-noise-protocol/src/transport.rs
+++ crates/bw-noise-protocol/src/transport.rs
@@ -7,7 +7,7 @@
 //! Replay protection is implemented using timestamps and a buffer of seen nonces.
 
 use std::collections::BTreeMap;
-#[cfg(not(test))]
+#[cfg(not(any(test, target_arch = "wasm32")))]
 use std::time::{SystemTime, UNIX_EPOCH};
 
 use chacha20poly1305::{
@@ -81,12 +81,17 @@ impl Timeprovider {
         }
     }
 
-    #[cfg(not(test))]
+    #[cfg(not(any(test, target_arch = "wasm32")))]
     fn now(&self) -> u64 {
         SystemTime::now()
             .duration_since(UNIX_EPOCH)
             .expect("System time before Unix epoch")
             .as_secs()
+    }
+
+    #[cfg(all(not(test), target_arch = "wasm32"))]
+    fn now(&self) -> u64 {
+        (js_sys::Date::now() / 1000.0) as u64
     }
 
     #[cfg(test)]
